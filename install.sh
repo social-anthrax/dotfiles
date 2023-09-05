0663a3e7828f9599488d7a950a9446ca61eeca30
Artemis Livingstone <ar.d.livingstone@gmail.com>
2023-09-05 00:21:50 -0400

    add local.toml generation

--- install.sh
+++ install.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env fish
 
-argparse --name="install.sh" "poetry" "pwn_env" -- $argv
+argparse --name="install.sh" poetry pwn_env -- $argv
+
+if not test -e ./.dotter/local.toml
+    set -l local_toml (echo """includes = []
+    packages = ["default"]
+
+    [files]
+
+    [variables]
+    """ | string split \n | string trim -l | string collect)
+
+    echo $local_toml >./.dotter/local.toml
+end
 
 # Install configs with dotter
-if test (arch) = "arm64"
+if test (arch) = arm64
     ./dotter.arm
 else
     ./dotter
 end
 
-# Install fisher, but don't bother only run update as our fish_plugins already lists fisher.
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher update
 tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Lightest --show_time='24-hour format' --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_height='Two lines' --prompt_connection=Disconnected --powerline_prompt_frame='No frame' --prompt_spacing=Sparse --icons='Many icons'
 
 # Download pwn-env into ~/envs but don't automatically install it
 if set -q _flag_pwn_env
-    mkdir ~/envs; pushd ~/envs
+    mkdir ~/envs
+    pushd ~/envs
     git clone git@github.com:social-anthrax/pwn-env.git
 
     # Only install if configured to install
-    if set -q _flag_poetry 
+    if set -q _flag_poetry
         echo "Poetry flag set, attempting to use poetry"
         if command -qs poetry
             if not poetry install
