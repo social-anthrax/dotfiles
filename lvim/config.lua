6f12548795deeeac11fb79273d0798abbd4dff60
Artemis Livingstone <ar.d.livingstone@gmail.com>
2023-09-22 15:37:56 -0400

    Add clang-format to lunarvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -8,8 +8,22 @@
 lvim.lsp.buffer_mappings.normal_mode['gh'] = lvim.lsp.buffer_mappings.normal_mode['gs']
 
 -- Behaviour
-lvim.format_on_save.enabled = false
+lvim.format_on_save.enabled = true
 lvim.builtin.which_key.mappings["e"] = { ":NvimTreeFocus<CR>", "Explorer" }
+
+-- Linters
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    name = "clang_format",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespace
+    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+    filetypes = { "c", "cpp" },
+  },
+}
+
 
 -- Plugins
 lvim.plugins = {
