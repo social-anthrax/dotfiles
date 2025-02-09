89ecbba8bd4cc12cab9e33c7a2d3c87c6861457d
Artemis Livingstone <ar.d.livingstone@gmail.com>
2025-02-09 19:18:41 +0000

    Misc changes

--- fish/functions/scratch_space.fish
+++ fish/functions/scratch_space.fish
@@ -4,7 +4,7 @@ function scratch_space --description "Puts the user into a new tmp dir until exi
     set -l scratch_basename "./$(basename $scratch_space_dir)"
     ln -s "$scratch_space_dir" "$scratch_basename"
 
-    set -lx fish_greeting (echo -e "Scratching allowed, no biting. \nYou're going to ~~Brazil!~~ $scratch_space_dir" | string collect)
+    set -lx fish_greeting (echo -e "Scratching allowed, no biting. \nYou're going to \x1b[9mBrazil!\x1b[29m $scratch_space_dir" | string collect)
     fish -C "cd ./$scratch_basename"
 
     rm "./$scratch_basename"
